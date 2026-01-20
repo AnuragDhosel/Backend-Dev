83d60f4812343fad36cf5687618e645b1be87966
Anurag dhosel <anuragdhosel04@gmail.com>
2026-01-20 15:15:06 +0530

    Delete NodeJsProgramming directory

--- NodeJsProgramming/Exercise-2/testStringUtils.js
+++
@@ -1,7 +0,0 @@
-const { capitalize, reverseString, countVowels } = require("./stringUtils");
-
-let s = "hello world";
-
-console.log("Capital:", capitalize(s));
-console.log("Reverse:", reverseString(s));
-console.log("Vowels:", countVowels(s));
